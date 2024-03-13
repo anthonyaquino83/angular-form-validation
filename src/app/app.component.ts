8d9251a883c6008e924238232c1ee871b2f25c1c
anthonyaquino <anthonyjefferson@gmail.com>
2024-03-13 16:22:45 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroFormTemplateComponent } from './template/hero-form-template/hero-form-template.component';
+import { HeroFormReactiveComponent } from './reactive/hero-form-reactive/hero-form-reactive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroFormTemplateComponent, HeroFormReactiveComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-form-validation';
